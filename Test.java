0f26ea692488e23644d2c148d18dc736487e2296
L10TOYA <112649759+L10TOYA@users.noreply.github.com>
2026-02-18 20:27:31 -0500

    Update Test.java added 3 lines of code

--- Test.java
+++ Test.java
@@ -1,5 +1,11 @@
 class Test{
   Public static void main(String[] args){
     System.out.println("This is a test");
+
+    int x = 2;
+
+    if(x % 2 == 0){
+      System.out.println("even");
+    }
   }
 }
